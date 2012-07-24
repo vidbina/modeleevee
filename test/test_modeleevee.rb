592301c40e511560a53d70d601e4e06443f6a13c
David Asabina <david.asabina@gmail.com>
2012-07-24 18:47:49 +0200

    Have to clean up my \t's as I forgot to set expandtab in Vim couldn't reset and recommit as I've already pushed the tabbed versions to github. Yeah, Not a smart move -- I know.

--- test/test_modeleevee.rb
+++ test/test_modeleevee.rb
@@ -2,7 +2,7 @@ require "test/unit"
 require "modeleevee"
 
 class ModeleeveeTest < Test::Unit::TestCase
-	def test_origin
-		assert_equal "madeliefje", Modeleevee.origin
-	end
+  def test_origin
+    assert_equal "madeliefje", Modeleevee.origin
+  end
 end
